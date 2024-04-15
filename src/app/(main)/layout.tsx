23f4508e34bde6dc4a749047596dbaa3dc30ec4c
Mo Saleh <mohamed.is8890@gmail.com>
2024-04-15 07:18:15 +0200

    Landing page, dashboard sidebar

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+import Sidebar from "@/components/sidebar";
+
+type Props = { children: ReactNode };
+
+const Layout = (props: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+      <Sidebar />
+      <div className="w-full">{props.children}</div>
+    </div>
+  );
+};
+
+export default Layout;
